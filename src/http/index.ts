ffefd7ea03b003cb38dcc18fc79032a82f0f7216
zcd66666 <15168728806@163.com>
2023-02-18 09:41:07 +0800

    添加响应失败提示

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,14 @@
+/*
+ * @Author: zcd66666 15168728806@163.com
+ * @Date: 2023-01-06 09:47:45
+ * @LastEditors: zcd66666 15168728806@163.com
+ * @LastEditTime: 2023-02-18 09:35:33
+ * @FilePath: \suppliermateriallabelprintingtoolpage\src\http\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import axios from 'axios'
 // 创建一个 axios 实例
-
+import { ElMessage } from 'element-plus'
 function setBaseURL(){
     let baseURL="";
     if (process.env.NODE_ENV === 'production') {
@@ -39,6 +47,7 @@ service.interceptors.request.use(
     function (error) {
         // 对请求错误做些什么
         console.log(error)
+        ElMessage.error('请求地址有误')
         return Promise.reject(error)
     }
 )
@@ -50,6 +59,7 @@ service.interceptors.response.use(
     },
     function (error) {
         console.log(error)
+        ElMessage.error('签名无效或者已过期，请重新登录')
         return Promise.reject(error)
     }
 )
